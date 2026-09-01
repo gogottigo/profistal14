6c21cacba7fe844afb2b808d291a218c1ef40f1e
teddio <rahleevf@gmail.com>
2026-09-01 22:41:38 +0900

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gogottigo.github.io',
+  base: '/profistal14',
   vite: {
     plugins: [tailwindcss()]
   }
