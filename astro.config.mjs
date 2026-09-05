649f224a48470394334a49000caaa8388f88afed
teddio <rahleevf@gmail.com>
2026-09-05 22:33:30 +0900

    Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://profistal14.ru',
   base: '/',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
